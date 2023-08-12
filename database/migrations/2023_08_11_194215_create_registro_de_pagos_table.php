1b5cecc9554141a69cf5965a0787fa27746c16eb
gtmarcos <gtmarcosf96@gmail.com>
2023-08-11 20:11:41 -0500

    PruebaTecnicaLaravel1

---
+++ database/migrations/2023_08_11_194215_create_registro_de_pagos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registro_de_pagos', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('message');
+            $table->string('result');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registro_de_pagos');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\homeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', homeController::class);
+
+route::get('token', function(){
+    return "123456";
+});
+
+
